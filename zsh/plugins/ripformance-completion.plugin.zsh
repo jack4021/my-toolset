906285e88aad0d51f208d87b849bde6abb706367
Brandon Jackson <brandon.jackson@rusticisoftware.com>
2023-07-19 15:07:36 -0500

    adding ripformance completion

--- zsh/plugins/ripformance-completion.plugin.zsh
+++ zsh/plugins/ripformance-completion.plugin.zsh
@@ -1,7 +1,7 @@
 #compdef ripformance
 
 function _ripformance_completion() {
-  _arguments "--bail[bails at the first failure]" "--errors:[only logs errors]" "--xapiVersion=[the targeted xapi version]" "--endpoint=[the LRS endpoint to target]"
+  _arguments '--bail[bails at the first failure]' '--errors:[only logs errors]' '--xapiVersion=[the targeted xapi version]' '--endpoint=[the LRS endpoint to target]'
 }
 
 _ripformance_completion "$@"
